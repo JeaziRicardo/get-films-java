1238cf2afaa0a76c53ab4bf71469142df2a22426
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-03-30 20:20:16 -0300

    feat: instancia a classe JsonParser com o método parse

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
 
@@ -14,6 +16,8 @@ public class App {
         var request = HttpRequest.newBuilder(address).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
     }
 }
