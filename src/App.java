621e4082afabac3ec1f68efe6a3a578328fa1d03
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-03-30 20:21:24 -0300

    feat: exibe os dados filtrados

--- src/App.java
+++ src/App.java
@@ -19,5 +19,12 @@ public class App {
 
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
