2b16a4b4025969ac66061938e0b8b735bc3d1fe3
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-03-30 21:42:46 -0300

    feat: substitui a url da api pela variável de ambiente

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -5,12 +6,17 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+        var prop = new Properties();
 
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        prop.load(new FileInputStream("config.properties"));
+        String URL_API = prop.getProperty("URL_API");
+
+        String url = URL_API;
         URI address = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(address).GET().build();
