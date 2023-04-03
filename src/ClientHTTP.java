75244b882973334f80f154e0c5706a866f2a06b1
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-03 20:11:57 -0300

    feat: cria a classe ClientHTTP

---
+++ src/ClientHTTP.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ClientHTTP {
+    public String getData(String url) {
+
+        try {
+            URI address = URI.create(url);
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(address).GET().build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            String body = response.body();
+            return body;
+        } catch (IOException | InterruptedException ex) {
+            throw new ClientHttpException("Erro ao consultar a URL. :(");
+        }
+
+    }
+}
