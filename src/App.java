f1def89c4e7bc6ab1cf68295014137ad0fe1d088
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-03 20:12:12 -0300

    feat: refatora a classe App

--- src/App.java
+++ src/App.java
@@ -1,9 +1,4 @@
 import java.io.FileInputStream;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -12,36 +7,24 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         var prop = new Properties();
+        var star = new Star();
+        var client = new ClientHTTP();
 
         prop.load(new FileInputStream("config.properties"));
         String URL_API = prop.getProperty("URL_API");
 
         String url = URL_API;
-        URI address = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(address).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        var body = client.getData(url);
 
         var parser = new JsonParser();
-        List<Map<String, String>> filmsList = parser.parse(body);
+        List<Map<String, String>> films = parser.parse(body);
 
-        for (Map<String,String> film : filmsList) {
+        for (Map<String,String> film : films) {
             System.out.format("\u001b[1m\u001b[92m\u001b[103m%s \u001b[m \n", film.get("title"));
             System.out.println(film.get("image"));
-            System.out.println(star(film.get("imDbRating")));
+            System.out.println(star.starring(film.get("imDbRating")));
             System.out.println(film.get("imDbRating"));
             System.out.println();
         }
-    }
-
-    static String star(String rating) {
-        var star = "";
-        var ratingString = Double.parseDouble(rating);
-        var ratingInt = (int) ratingString;
-        for (int i = 0; i < ratingInt; i++) {
-            star += "\u2B50";
-        }
-        return star;
     }
 }
